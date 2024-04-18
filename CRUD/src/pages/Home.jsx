5d4538729fd0bfc1e5945f777b38d8644bc47c1f
Sudam-Praveen <sudam.peramuna@gmail.com>
2024-04-18 22:51:46 +0530

    displaying user detail using axios and set the users array then get the data from the array to a table

--- CRUD/src/pages/Home.jsx
+++ CRUD/src/pages/Home.jsx
@@ -1,36 +1,46 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 export default function Home() {
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        loadUsers();
+
+    }, []);
+
+    const loadUsers = async () => {
+        const results = await axios.get('http://localhost:8080/getUsers')
+        console.log(results.data);
+        setUsers(results.data)
+    }
+
     return (
         <div className='container'>
             <div className="py-4">
-                <table className="table">
+                <table className="table border shadow">
                     <thead>
                         <tr>
-                            <th scope="col">#</th>
-                            <th scope="col">First</th>
-                            <th scope="col">Last</th>
-                            <th scope="col">Handle</th>
+                            <th scope="col">No</th>
+                            <th scope="col">Name</th>
+                            <th scope="col">UserName</th>
+                            <th scope="col">Email</th>
+                            <th scope="col">Option</th>
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <th scope="row">1</th>
-                            <td>Mark</td>
-                            <td>Otto</td>
-                            <td>@mdo</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">2</th>
-                            <td>Jacob</td>
-                            <td>Thornton</td>
-                            <td>@fat</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">3</th>
-                            <td colspan="2">Larry the Bird</td>
-                            <td>@twitter</td>
-                        </tr>
+                        {
+                            users.map((user, index) => (
+                                <tr>
+                                    <th scope="row" key={index}>{index + 1}</th>
+                                    <td>{user.name}</td>
+                                    <td>{user.userName}</td>
+                                    <td>{user.email}</td>
+                                </tr>
+                            ))
+                        }
+
+
                     </tbody>
                 </table>
             </div>
