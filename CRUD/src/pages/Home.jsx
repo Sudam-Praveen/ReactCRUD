641d9d5872e74c186e534a26d5d5ce149afecf0f
Sudam-Praveen <sudam.peramuna@gmail.com>
2024-04-18 23:09:10 +0530

    installed react-router-dom for routing

--- CRUD/src/pages/Home.jsx
+++ CRUD/src/pages/Home.jsx
@@ -1,49 +1,51 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 
-export default function Home() {
+export default function () {
+
     const [users, setUsers] = useState([]);
 
     useEffect(() => {
         loadUsers();
-
-    }, []);
+    }, [])
 
     const loadUsers = async () => {
-        const results = await axios.get('http://localhost:8080/getUsers')
+        const results = await axios.get("http://localhost:8080/getUsers");
         console.log(results.data);
         setUsers(results.data)
     }
 
     return (
-        <div className='container'>
-            <div className="py-4">
-                <table className="table border shadow">
-                    <thead>
-                        <tr>
-                            <th scope="col">No</th>
-                            <th scope="col">Name</th>
-                            <th scope="col">UserName</th>
-                            <th scope="col">Email</th>
-                            <th scope="col">Option</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
-                            users.map((user, index) => (
-                                <tr>
-                                    <th scope="row" key={index}>{index + 1}</th>
-                                    <td>{user.name}</td>
-                                    <td>{user.userName}</td>
-                                    <td>{user.email}</td>
-                                </tr>
-                            ))
-                        }
+        <div>
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">No</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">UserName</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">Option</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        users.map((user, index) => (
+                            <tr>
+                                <th scope="row" key={index}>{index + 1}</th>
+                                <td>{user.name}</td>
+                                <td>{user.userName}</td>
+                                <td>{user.email}</td>
+                                <td>
+                                    <button className='btn btn-primary mx-2'>View</button>
+                                    <button className='btn btn-outline-primary mx-2'>Edit</button>
+                                    <button className='btn btn-danger mx-2'>Delete</button>
+                                </td>
+                            </tr>
+                        ))
+                    }
 
-
-                    </tbody>
-                </table>
-            </div>
+                </tbody>
+            </table>
         </div>
     )
 }
